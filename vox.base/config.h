b32a12f4868dd96fb74dee7a71b21bd004a95915
yangfengzzz <yangfengzzz@hotmail.com>
2022-08-13 18:25:57 +0800

    add compact search

---
+++ vox.base/config.h
@@ -0,0 +1,18 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#pragma once
+
+namespace CompactNSearch {
+#ifdef USE_DOUBLE
+using Real = double;
+#else
+using Real = float;
+#endif
+} // namespace CompactNSearch
+
+#define INITIAL_NUMBER_OF_INDICES 50
+#define INITIAL_NUMBER_OF_NEIGHBORS 50
